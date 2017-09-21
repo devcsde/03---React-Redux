645dc872041951d043c0581f9eb0cd4ffbeaa6db
devcs <c.scheidler@gmx.net>
2017-09-21 02:24:00 +0200

    Add redux-form, make header, make posts/new Route and Form validation with conditional error rendering

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,16 @@ import {Link} from "react-router-dom";
 export default class Header extends Component {
     render() {
         return (
-            <div className="container devheader">
-                <h2>devblog</h2>
-                <hr/>
-                <div className="text-xs-right">
+            <div className="row container devheader">
+                <div className="col-md-6">
+                    <h2>devblog</h2>
+                    <hr/>
+                </div>
+                <div className="col-md-6 text-xs-right">
                     <Link className="btn btn-secondary btn-sm" to="/posts/new">
                         Add a Post
                     </Link>
+                    <hr/>
                 </div>
             </div>
         );
--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,15 +1,89 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
+
 // import {connect} from "react-redux";
 
-class PostsNew extends React.Component{
+class PostsNew extends React.Component {
+    renderField(field) {  // field comes as object from redux-form
+        const {meta: {touched, error}} = field; // field.meta.touched destructuring
+        const className = `form-group ${touched && error ? "has-danger" : ""}`;
+        return (
+            <div className={className}>
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+                <p className="text-help">{touched ? error : ""}</p>
+            </div>
+        )
+    }
+
+    onSubmit(values){
+        console.log(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props; // handleSubmit prop comes from redux-form
+
         return (
             <div className="container">
-                PostsNew!
+                <form className="form" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                    <Field
+                        label="Title"
+                        name="title"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Category"
+                        name="categories"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Content"
+                        name="content"
+                        component={(field) => {
+                            const {meta: {touched, error}} = field; // field.meta.touched destructuring
+                            const className = `form-group ${touched && error ? "has-danger" : ""}`;
+                            return (
+                                <div className={className}>
+                                    <label>{field.label}</label>
+                                    <textarea
+                                        className="form-control"
+                                        type="text"
+                                        {...field.input}
+                                    />
+                                    <p className="text-help">{touched ? error : ""}</p>
+                                </div>
+                            )
+                        }}
+                    />
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
             </div>
         )
     }
 }
 
-export default PostsNew;
+function validate(values){
+    const errors = {};
+
+    if(!values.title || values.title.length < 3){
+        errors.title = "Enter a title that has at least 3 characters!"
+    }
+    if (!values.categories){
+        errors.categories = "Enter a category, or categories!"
+    }
+    if (!values.content) {
+        errors.content = "Enter some content!"
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: "PostNewForm"
+})(PostsNew);
 
